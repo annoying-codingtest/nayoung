417728027d011bbf332473628cb521d14de2e8c0
nyny1000 <rnjsskdud941@naver.com>
2021-10-10 17:45:41 +0900

    백트래킹 스타트와링크 아직안됨

--- backtracking/src/backtracking/Bj14889_스타트와링크.java
+++ backtracking/src/backtracking/Bj14889_스타트와링크.java
@@ -9,6 +9,9 @@ public class Bj14889_스타트와링크 {
 
 	public static int N;
 	public static int[][] map;
+	public static int[] startTeam;
+	public static int[] linkTeam;
+
 	public static boolean[] visit;
 
 	public static int min = Integer.MAX_VALUE;
@@ -21,6 +24,8 @@ public class Bj14889_스타트와링크 {
 
 		map = new int[N][N];
 		visit = new boolean[N];
+		startTeam = new int[N/2];
+		linkTeam = new int[N/2];
 
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -30,56 +35,14 @@ public class Bj14889_스타트와링크 {
 			}
 		}
 
-		dfs(0,0);
+		dfs(0);
 		System.out.println(min);
 	}
 
-	private static void dfs(int idx, int d) {
-		/*
-		 * start팀과 link팀은 구분만 하면 되기 때문에 
-		 * visit배열의 값이 true인 팀 / false인 팀으로 나누기
-		 * 
-		 */
-		if (d == N / 2) {
-			diff_status();
-			return;
-		} else {
-			for (int i = idx; i < N; i++) {  //i=0부터 하면 시간초과
-				if (visit[i] == false) {
-					visit[i] = true;
-					dfs(i+1, d + 1);
-					// 백 트래킹
-					visit[i] = false;
-				}
-			}
-		}
+	private static void dfs(int d) {
 
 	}
 
 	static void diff_status() {
-		int startTeam = 0; // 스타트팀의 총 능력치
-		int linkTeam = 0; // 링크팀의 총 능력치
-
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = i + 1; j < N; j++) {
-				// i 번째 사람과 j 번째 사람이 true다 -> 스타트팀의 능력치에 더하기
-				if (visit[i] == true && visit[j] == true) {
-					startTeam += map[i][j];
-					startTeam += map[j][i];
-				}
-				// i 번째 사람과 j 번째 사람이 false다 -> 링크팀의 능력치에 더하기
-				else if (visit[i] == false && visit[j] == false) {
-					linkTeam += map[i][j];
-					linkTeam += map[j][i];
-				}
-			}
-		}
-		// 두 팀의 점수 차
-		int diff = Math.abs(startTeam - linkTeam);
-		
-		// 각 팀 조합의 경우마다 능력치 합의 최솟값 갱신
-		min = Math.min(diff, min);
-
 	}
-
 }
