247bedfa4a1509b5e1a9011271d11e2b346923ab
nyny1000 <rnjsskdud941@naver.com>
2021-10-10 17:30:26 +0900

    백트래킹 스도쿠

--- backtracking/src/backtracking/Bj2580_스도쿠.java
+++ backtracking/src/backtracking/Bj2580_스도쿠.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Bj2580_½ºµµÄí {
 
-	public static int[][] arr = new int[9][9];
+	public static int[][] arr = new int[9][9]; 
 
 	public static void main(String[] args) throws IOException {
 
